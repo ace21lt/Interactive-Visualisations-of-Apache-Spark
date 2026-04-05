d4211b3f2d9980dfcf23d624d1d9e97970435e82
Liam Thomas <lthomas13@sheffield.ac.uk>
2026-04-05 18:55:56 +0100

    changed the dockerfile to work with railway to be deployed and make it a single container for both fronttend and backend and added a toml file

--- src/main/scala/config/ConfigReader.scala
+++ src/main/scala/config/ConfigReader.scala
@@ -7,12 +7,12 @@ object ConfigReader:
 
   // Read optional string environment variable
   def getOptionalEnv(name: String): UIO[Option[String]] =
-    ZIO.succeed(sys.env.get(name).map(_.trim).filter(_.nonEmpty))
+    ZIO.succeed(scala.sys.env.get(name).map(_.trim).filter(_.nonEmpty))
 
   // Read integer environment variable with bounds checking
   def getOptionalEnvInt(name: String, default: Int, min: Int, max: Int): UIO[Int] =
     ZIO.succeed {
-      sys.env.get(name).flatMap(_.toIntOption) match
+      scala.sys.env.get(name).flatMap(_.toIntOption) match
         case Some(v) if v >= min && v <= max => v
         case _                               => default
     }
@@ -20,7 +20,7 @@ object ConfigReader:
   // Read double environment variable with bounds checking
   def getOptionalEnvDouble(name: String, default: Double, min: Double, max: Double): UIO[Double] =
     ZIO.succeed {
-      sys.env.get(name).flatMap(_.toDoubleOption) match
+      scala.sys.env.get(name).flatMap(_.toDoubleOption) match
         case Some(v) if v >= min && v <= max => v
         case _                               => default
     }
@@ -28,7 +28,7 @@ object ConfigReader:
   // Read boolean environment variable
   def getOptionalEnvBoolean(name: String, default: Boolean): UIO[Boolean] =
     ZIO.succeed {
-      sys.env.get(name).map(_.toLowerCase) match
+      scala.sys.env.get(name).map(_.toLowerCase) match
         case Some("true") | Some("1") | Some("yes") => true
         case Some("false") | Some("0") | Some("no") => false
         case _                                      => default
