043454094d00661cc56561caab056dbf68d1d4fd
Liam Thomas <124939649+ace21lt@users.noreply.github.com>
2026-05-03 15:32:08 +0100

    Add newline at end of Lab2ClusterView.js
    Fix formatting by adding a newline at the end of the file.

--- frontend/src/components/visualisations/Lab2ClusterView.js
+++ frontend/src/components/visualisations/Lab2ClusterView.js
@@ -7,7 +7,7 @@ import {
     brandPurpleDark,
     okabeIto,
     neutralSurface,
-    neutralWhite, chartBlueTint, chartPurpleTint
+    neutralWhite, chartBlueTint, chartPurpleTint,
 } from '../../theme/palette';
 
 // Okabe-Ito colour palette
